3800dd3233843dfcfb5ff707c86f3a42ea89bcf5
Cam Jackson <1930451+camjackson@users.noreply.github.com>
2023-01-28 11:32:08 +1100

    Start laying out the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <main className="mx-10 mt-4">Hey</main>;
+  return (
+    <main className="max-w-5xl h-full mx-auto bg-white shadow-2xl px-10 pt-5">
+      <h1 className="text-3xl mb-3">D&D Prints</h1>
+      <p>
+        Create printable thingies for your D&D table. PC name cards, initiative
+        tokens, etc.
+      </p>
+    </main>
+  );
 };
 
 export default App;
